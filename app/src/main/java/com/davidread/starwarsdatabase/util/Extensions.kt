b5902f5b38b0860586960c33f5901de3a9776871
David Read <49120229+david-read-iii@users.noreply.github.com>
2023-01-16 23:07:41 -0500

    Add one extension function for extracting a List of Int ids from a List of String SWAPI URLs. Add another extension function for converting a List of ResourceResponse into a String list representation of all the names/titles of the elements of the original List.

--- app/src/main/java/com/davidread/starwarsdatabase/util/Extensions.kt
+++ app/src/main/java/com/davidread/starwarsdatabase/util/Extensions.kt
@@ -1,5 +1,6 @@
 package com.davidread.starwarsdatabase.util
 
+import com.davidread.starwarsdatabase.model.datasource.ResourceResponse
 import java.util.regex.Pattern
 
 /**
@@ -18,4 +19,46 @@ fun String.extractIDFromURL(): Int {
     } else {
         throw IllegalArgumentException("$this must match the regular expression $regex")
     }
+}
+
+/**
+ * Takes a [List] of [String], where each matches the URL `https://swapi.dev/api/{endpoint}/{id}/`
+ * and each `{id}` field as an [Int] in a [List].
+ *
+ * @throws IllegalArgumentException Thrown when at least one [String] does not match the URL regular
+ * expression.
+ */
+fun List<String>.extractIDsFromURLs(): List<Int> {
+    val regex = "https://swapi\\.dev/api/[a-z]+/(\\d+)/"
+    val pattern = Pattern.compile(regex)
+    val idList = mutableListOf<Int>()
+    this.forEach { url ->
+        val matcher = pattern.matcher(url)
+        if (matcher.matches()) {
+            idList.add(matcher.group(1)!!.toInt())
+        } else {
+            throw IllegalArgumentException("$url must match the regular expression $regex")
+        }
+    }
+    return idList
+}
+
+/**
+ * Takes a [List] of [ResourceResponse] and returns a [String] comma-delimited list of each object's
+ * `name` or `title` field.
+ */
+fun List<ResourceResponse>.extractNames(): String {
+    val nameList = mutableListOf<String>()
+    this.forEach { resourceResponse ->
+        val name = when (resourceResponse) {
+            is ResourceResponse.Person -> resourceResponse.name
+            is ResourceResponse.Film -> resourceResponse.title
+            is ResourceResponse.Starship -> resourceResponse.name
+            is ResourceResponse.Vehicle -> resourceResponse.name
+            is ResourceResponse.Species -> resourceResponse.name
+            is ResourceResponse.Planet -> resourceResponse.name
+        }
+        nameList.add(name)
+    }
+    return nameList.toString().removeSurrounding("[", "]")
 }
