80be7c3d191cf86dcac6166689d351f4e3c73675
David Read <49120229+david-read-iii@users.noreply.github.com>
2023-02-05 12:41:52 -0500

    Move unit test rules into common BaseViewModelImplTest.kt for each ...ViewModelImplTest class to inherit.

---
+++ app/src/test/java/com/davidread/starwarsdatabase/viewmodel/BaseViewModelImplTest.kt
@@ -0,0 +1,29 @@
+package com.davidread.starwarsdatabase.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.davidread.starwarsdatabase.RxImmediateSchedulerRule
+import org.junit.Rule
+
+/**
+ * Base class for `...ViewModelImplTest` classes to inherit. Provides rules for the inheriting test
+ * class to use in order for it to conduct unit tests on `ViewModel` subclasses that use
+ * Architecture Components and RxJava.
+ */
+abstract class BaseViewModelImplTest {
+
+    /**
+     * Rule that swaps the background executor used by the Architecture Components with one that
+     * executes each task synchronously.
+     */
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    /**
+     * Rule that forces RxJava to execute it's load on the main thread.
+     */
+    @Rule
+    @JvmField
+    val testSchedulerRule = RxImmediateSchedulerRule()
+
+}
